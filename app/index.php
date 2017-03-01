a8ad15d9be39355a4e5f3427d537900416b9f872
Deffhyper <axelogykk@mail.ru>
2017-03-02 00:08:10 +0200

    Create main files

---
+++ app/index.php
@@ -0,0 +1,19 @@
+<?php include_once "parts/head.php"?>
+<body>
+	<header>
+	    <div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<?php echo "Hello" ?>
+				</div>
+				<div class="col-sm-9">
+					<?php echo "Best html preprocessor!!!" ?>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<?php include_once "parts/footer.php"?>
+
+</body>
+</html>
