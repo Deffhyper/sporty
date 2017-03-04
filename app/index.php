55942de760c7bf65b013d1ec836899ebbb442ef0
Deffhyper <axelogykk@mail.ru>
2017-03-04 18:23:13 +0200

    add section on index.php

--- app/index.php
+++ app/index.php
@@ -2,16 +2,240 @@
 <html>
 <?php include_once "parts/header.php" ?>
 
-    <div class="block-with-back">
+    <section class="site-top-filter">
+        <div class="container ">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 class="site-top-filter__title">Выберите спортивное направление <strong>в Киеве:</strong></h1>
 
-    </div>
-    <div class="container ">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class=""></div>
+                    <form action="#" id="" class="site-top-filter__form">
+                        <div class="form-group">
+                            <div class="form-group--item">
+                                <select class="custom-select left-round">
+                                    <option value="1">Спортивные объекты</option>
+                                    <option value="2">Тренеры</option>
+                                    <option value="3">Аренда</option>
+                                    <option value="4">Спортивные магазины</option>
+                                    <option value="5">Акции</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group--item">
+                                <select class="custom-select no-round">
+                                    <option value="1">Выберите направление спорта</option>
+                                    <option value="2">Футбол</option>
+                                    <option value="3">Волейбол</option>
+                                    <option value="4">Велоспорт</option>
+                                    <option value="5">Дзю До</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group--item">
+                                <select class="custom-select right-round">
+                                    <option value="1">Выберите район</option>
+                                    <option value="2">Днепровский</option>
+                                    <option value="3">Соломенский</option>
+                                    <option value="4">Оболонский</option>
+                                    <option value="5">Подолский</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn filter-button">Найти</button>
+                    </form>
+                </div>
+            </div>
+            <div class="row">
+                <ul class="indicator">
+                    <li class="indicator-item">
+                        <p class="indicator-item--title">17 000</p>
+                        <p class="indicator-item--data">Залов</p>
+                    </li>
+                    <li class="indicator-item">
+                        <p class="indicator-item--title">25 786</p>
+                        <p class="indicator-item--data">Тренеров</p>
+                    </li>
+                    <li class="indicator-item">
+                        <p class="indicator-item--title">12 000</p>
+                        <p class="indicator-item--data">Товаров</p>
+                    </li>
+                    <li class="indicator-item">
+                        <p class="indicator-item--title">1 000 000</p>
+                        <p class="indicator-item--data">Посетителей</p>
+                    </li>
+                </ul>
             </div>
         </div>
-    </div>
+    </section>
+
+    <section class="section-block md-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="section-block__title">Популярные секции Киева</h4>
+                    <ul class="link-list">
+                        <li class="link-list__item"><a href="#" class="link">PRO Jumping TRX<span>(40)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Zumba <span>(40)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Автоспорт и мотоспорт<span>(140)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Айкидо<span>(40)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Аквааэробика<span>(10)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Авиационный клуб <span>(30)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Гольф клуб<span>(1)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Ипподром, конный клуб<span>(2)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Боулинг клуб<span>(8)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Йога центр<span>(15)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Тир, стрелковый клуб<span>(2)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Яхтинг клуб<span>(2)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Школа боевых искусств<span>(54)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Фитнес клуб<span>(140)</span></a></li>
+                        <li class="link-list__item"><a href="#" class="link">Экстрим<span>(2)</span></a></li>
+                    </ul>
+                    <div class="section-block__link">
+                        <a href="#" class="link-button">Все виды спорта</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="block-with-back section-block md-padding">
+        <div class="container ">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="section-block__title">Спортивные акции Киев</h4>
+                    <div class="slider-five">
+                        <div>
+                            <div class="slider-five__item">
+                                <div class="slider-five__item--foto">
+                                    <a href="#">
+                                        <span class="icon-sticker"><span>11%</span></span>
+                                        <img src="img/jpeg_files/slider-foto-1.png" alt="picture">
+                                    </a>
+                                </div>
+                                <div class="slider-five__item--link">
+                                    <a href="#">Скидка 50% на все абонементы</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div>
+                            <div class="slider-five__item">
+                                <div class="slider-five__item--foto">
+                                    <a href="#">
+                                        <span class="icon-sticker"><span>11%</span></span>
+                                        <img src="img/jpeg_files/slider-foto-2.png" alt="picture">
+                                    </a>
+                                </div>
+                                <div class="slider-five__item--link">
+                                    <a href="#">Скидка 50% на все абонементы</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div>
+                            <div class="slider-five__item">
+                                <div class="slider-five__item--foto">
+                                    <a href="#">
+                                        <span class="icon-sticker"><span>11%</span></span>
+                                        <img src="img/jpeg_files/slider-foto-3.png" alt="picture">
+                                    </a>
+                                </div>
+                                <div class="slider-five__item--link">
+                                    <a href="#">Скидка 50% на все абонементы</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div>
+                            <div class="slider-five__item">
+                                <div class="slider-five__item--foto">
+                                    <a href="#">
+                                        <span class="icon-sticker"><span>11%</span></span>
+                                        <img src="img/jpeg_files/slider-foto-4.png" alt="picture">
+                                    </a>
+                                </div>
+                                <div class="slider-five__item--link">
+                                    <a href="#">Скидка 50% на все абонементы</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div>
+                            <div class="slider-five__item">
+                                <div class="slider-five__item--foto">
+                                    <a href="#">
+                                        <span class="icon-sticker"><span>11%</span></span>
+                                        <img src="img/jpeg_files/slider-foto-5.png" alt="picture">
+                                    </a>
+                                </div>
+                                <div class="slider-five__item--link">
+                                    <a href="#">Скидка 50% на все абонементы</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div>
+                            <div class="slider-five__item">
+                                <div class="slider-five__item--foto">
+                                    <a href="#">
+                                        <span class="icon-sticker"><span>11%</span></span>
+                                        <img src="img/jpeg_files/slider-foto-1.png" alt="picture">
+                                    </a>
+                                </div>
+                                <div class="slider-five__item--link">
+                                    <a href="#">Скидка 50% на все абонементы</a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section-block__link">
+                        <a href="#" class="link-button">Все акции</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-block md-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="section-block__title">Спортивные новости Киев</h4>
+                    <div class="section-block__link">
+                        <a href="#" class="link-button">Все новости</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-block md-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="section-block__title">О проекте Sporty</h4>
+                    <p class="section-block__text">
+                        Cпорт сегодня занимает достаточно много времени в общем объеме интересов человечества.
+                        Каждого из нас привлекает какой то вид спорта, будь то футбол, хоккей, теннис, бокс, баксетбол или
+                        биатлон. Наш ресурс предлагает возможность получить полнейшую информацию, статистику, результаты в
+                        таких разделах мирового спорта, как: футбол онлайн, теннис онлайн, хоккей онлайн,биатлон онлайн и весь
+                        спорт в целом.
+                    </p>
+                    <p class="section-block__text">
+                        Мы постараемся найти для вас интересные спортивные события и прямые трансляции трансляции онлайн.
+                        Также позаботимся о возможности просмотра видео повторов голов, обзоры матчей и самые интересные
+                        спортивные моменты. У нас вы сможете принять участие в конкурсе прогнозов, проанализировать турнирные
+                        таблицы лучших чемпионатов, узнать результаты спортивных соревнований, быть в курсе последних новостей
+                        спорта.
+                    </p>
+                    <p class="section-block__text">
+                        Спасибо, что вы с нами!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
 
 <?php include_once "parts/footer.php" ?>
 </html>
